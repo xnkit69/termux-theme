8bc63833b18a303a125172c3c205fd1f04e2cb18
ANKIT <xnkitk69@gmail.com>
2023-06-18 15:15:46 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+chmod +x ViewBanner
+chmod +x TermuxStyle
+cd /data/data/com.termux/files/usr/etc
+rm -f bash.bashrc motd
+
+pkg list-installed > log
+
+grep "wget" log
+if [ $? = 0 ]; then
+   echo -e "\e[92mWget exist"
+elif [ $? = 1 ]; then
+   pkg install wget -y
+else
+   echo -e "\e[91mUnknow Error\e[0m"
+fi
+
+while [ ! -e "bash.bashrc" ]
+do
+   wget https://raw.githubusercontent.com/xnkit69/z/zCT/bash.bashrc
+done
+
+grep "figlet" log
+if [ $? = 0 ]; then
+   echo "done"
+elif [ $? = 1 ]; then 
+   pkg install figlet -y
+else
+   echo -e "\e[91mUnknown Error\e[0m"
+fi
+
+rm -f log
+
+cd $HOME/CustomizeTermux
